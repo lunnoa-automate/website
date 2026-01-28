22be00e4b308f974f40c2f8af97306f8f53a9d2b
skelebuda <sasakelebuda@lunnoalabs.ch>
2026-01-28 09:01:20 +0100

    new logo

--- app/datenschutz/layout.jsx
+++ app/datenschutz/layout.jsx
@@ -1,6 +1,6 @@
 export const metadata = {
   title: 'Datenschutzerklärung - Lunnoa Automate',
-  description: 'Datenschutzerklärung der Lunnoa GmbH. Erfahren Sie, wie wir Ihre Daten schützen und verwenden. ',
+  description: 'Datenschutzerklärung der Lunnoa GmbH. Erfahren Sie, wie wir Ihre Daten schützen und verwenden.',
 };
 
 export default function DatenschutzLayout({ children }) {
